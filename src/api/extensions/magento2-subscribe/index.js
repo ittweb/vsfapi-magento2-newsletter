3cf11421542d58dc01d6a03913d424006db02062
Roberto Bianchi <rbianchi@ittweb.net>
2020-07-24 18:31:24 +0200

    Add GET and DELETE method

--- src/api/extensions/magento2-subscribe/index.js
+++ src/api/extensions/magento2-subscribe/index.js
@@ -9,16 +9,38 @@ const request = require('request')
 
 module.exports = ({ config }) => {
 
-	let m2Api = Router();
+  let m2Api = Router();
+  
+  /**
+   * GET user subscription
+   */
+  m2Api.get('/subscribe', (req, res) => {
+    let userEmail = req.query.email
+		if(!userEmail) {
+			apiStatus(res, 'Email not provided!', 500)
+			return
+		}
+    return request({
+      url: config.magento2.api.url + '/V1/newsletter/subscription/' + userEmail,
+      method: 'GET',
+      json: false
+    }, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        console.error(error, body)
+        apiStatus(res, 'An error occured while accessing Magento 2', 500)
+      } else {
+        apiStatus(res, body.status, 200)
+      }
+    })
+  })
 
 	/**
 	 * POST subscribe a user
 	 */
 	m2Api.post('/subscribe', (req, res) => {
-
 		let userEmail = req.body.email
 		if(!userEmail) {
-			apiStatus(res, 'No e-mail provided!', 500)
+			apiStatus(res, 'Email not provided!', 500)
 			return
 		}
     request({
@@ -26,9 +48,32 @@ module.exports = ({ config }) => {
       method: 'POST',
       json: false
     }, function (error, response, body) {
-      if (error) {
+      if (error || response.statusCode !== 200) {
         console.error(error)
-        apiStatus(res, 'An error occured while accessing Magento2', 500)
+        apiStatus(res, 'An error occured while accessing Magento 2', 500)
+      } else {
+        apiStatus(res, body.status, 200)
+      }
+    })
+  })
+  
+  /**
+	 * DELETE subscribe a user
+	 */
+	m2Api.delete('/subscribe', (req, res) => {
+		let userEmail = req.body.email
+		if(!userEmail) {
+			apiStatus(res, 'Email not provided!', 500)
+			return
+		}
+    request({
+      url: config.magento2.api.url + '/V1/newsletter/subscription/' + userEmail,
+      method: 'DELETE',
+      json: false
+    }, function (error, response, body) {
+      if (error || response.statusCode !== 200) {
+        console.error(error)
+        apiStatus(res, 'An error occured while accessing Magento 2', 500)
       } else {
         apiStatus(res, body.status, 200)
       }
